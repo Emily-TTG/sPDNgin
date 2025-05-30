42cdca977e0b47609e443ff2f3edf8ca1aa42369
Emily Banerjee <prs.ttg+gh@pm.me>
2025-05-31 00:42:15 +0100

    res: add basic tiled project

---
+++ res/tile/Goop.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.11.2",
+  name = "Goop",
+  class = "",
+  tilewidth = 16,
+  tileheight = 16,
+  spacing = 0,
+  margin = 0,
+  columns = 2,
+  image = "../img/Goop.png",
+  imagewidth = 32,
+  imageheight = 32,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 16,
+    height = 16
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 4,
+  tiles = {}
+}
---
+++ res/tile/src/Goop.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Goop" tilewidth="16" tileheight="16" tilecount="4" columns="2">
+ <transformations hflip="1" vflip="1" rotate="1" preferuntransformed="1"/>
+ <image source="../../img/Goop.png" width="32" height="32"/>
+</tileset>
